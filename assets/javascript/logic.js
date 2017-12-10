ee9e8035a686f6efd81c68a7ff5e2f9fc5c3ab1f
chris <chrisb000@gmail.com>
2017-12-10 11:51:11 -0800

    commits for the commits god

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -5,6 +5,15 @@ $(document).ready(function () {
         event.preventDefault();
         main.addTrain();
     });
+    main.database.ref().on("child_added", function (childSnapshot) {
+
+        main.populate(childSnapshot);
+
+        // Handle the errors
+    }, function (errorObject) {
+        console.log("Errors handled: " + errorObject.code);
+    });
+
 
 });
 
@@ -33,18 +42,35 @@ var main = {
         var name = $("#name").val();
         var dest = $("#dest").val();
         var time = $("#time").val();
-        // var freq = $("#freq").val();
+        var freq = $("#freq").val();
         console.log(name);
         console.log(dest);
         console.log(time);
-        // console.log(freq);
+        console.log(freq);
 
-        main.database.ref().set({
+        main.database.ref().push({
             Name: name,
             Dest: dest,
             Time: time,
-            // freq: freq
+            Freq: freq
         });
+    },
 
+    populate: (childSnapshot) => {
+
+        // Log everything that's coming out of snapshot
+        console.log(childSnapshot.val().Name);
+        console.log(childSnapshot.val().Dest);
+        console.log(childSnapshot.val().Time);
+        console.log(childSnapshot.val().Freq);
+
+        // full list of items to the well
+        $("#trains").append(
+            "<tr><td> " + childSnapshot.val().Name +
+            " </td><td> " + childSnapshot.val().Dest +
+            " </td><td> " + childSnapshot.val().Time +
+            " </td><td> " + childSnapshot.val().Freq +
+            " </td></tr>"
+        );
     }
 }
