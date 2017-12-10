31ae1cfc33846d12c8be927bc72607e42e4fa0f3
chris <chrisb000@gmail.com>
2017-12-10 13:57:19 -0800

    "added" timing events

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,24 +1,38 @@
+
+
+// On page load
 $(document).ready(function () {
+
+    // Initialize Firebase
     main.initFirebase();
-    console.log(main.database);
+
+    // When add train is clicked
     $("#addTrain").on("click", function (event) {
+
+        // Prevent refresh
         event.preventDefault();
+
+        // Add a train
         main.addTrain();
+
     });
+
+    // When a train is added
     main.database.ref().on("child_added", function (childSnapshot) {
 
+        // Populate the Train table
         main.populate(childSnapshot);
 
         // Handle the errors
     }, function (errorObject) {
         console.log("Errors handled: " + errorObject.code);
     });
-
-
 });
 
+// Object containing logic and references
 var main = {
 
+    // Holds the database reference
     database: "",
 
     // Initialize Firebase
@@ -31,23 +45,46 @@ var main = {
             storageBucket: "test-4998f.appspot.com",
             messagingSenderId: "718765377102"
         };
-
         firebase.initializeApp(config);
 
+        // Store the database reference to the object parameter
         main.database = firebase.database();
     },
 
+    // Adds a train to the database
     addTrain: function () {
 
-        var name = $("#name").val();
-        var dest = $("#dest").val();
-        var time = $("#time").val();
-        var freq = $("#freq").val();
-        console.log(name);
-        console.log(dest);
-        console.log(time);
-        console.log(freq);
+        // Grabs inputs from text fields
 
+
+        if ($("#name").val()) {
+            var name = $("#name").val();
+        } else {
+            alert("all fields required");
+            return
+        };
+        if ($("#freq").val()) {
+            var freq = $("#freq").val();
+        } else {
+            alert("all fields required");
+            return
+        };
+        if ($("#dest").val()) {
+            var dest = $("#dest").val();
+        } else {
+            alert("all fields required");
+            return
+        };
+
+        if ($("#time").val()) {
+            var time = $("#time").val();
+
+        } else {
+            alert("all fields required");
+            return
+        };
+
+        // Pushes values to database
         main.database.ref().push({
             Name: name,
             Dest: dest,
@@ -56,21 +93,51 @@ var main = {
         });
     },
 
+    // Fills the train table with all trains in the database
     populate: (childSnapshot) => {
 
         // Log everything that's coming out of snapshot
-        console.log(childSnapshot.val().Name);
-        console.log(childSnapshot.val().Dest);
-        console.log(childSnapshot.val().Time);
-        console.log(childSnapshot.val().Freq);
+        // console.log(childSnapshot.val().Name);
+        // console.log(childSnapshot.val().Dest);
+        // console.log(childSnapshot.val().Time);
+        // console.log(childSnapshot.val().Freq);
 
-        // full list of items to the well
+        let tFrequency = childSnapshot.val().Freq;
+        
+        let firstTime = childSnapshot.val().Time;
+        // var time = moment(temp).format("HH:mm");
+        console.log(firstTime);
+        let convert = moment(firstTime, "HH:mm").subtract(1, "years");
+        console.log(convert._i);
+        let firstTimeConverted = moment(convert._i, "HHmm").format("HH:mm");
+        // console.log(moment(firstTimeConverted._i).format("HH:mm"));
+        console.log(firstTimeConverted);
+        let currentTime = moment();
+        console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+
+        // Difference between the times
+        let diffTime = moment().diff(moment(convert), "minutes");
+        console.log("DIFFERENCE IN TIME: " + diffTime);
+
+        // Time apart (remainder)
+        let tRemainder = diffTime % tFrequency;
+        console.log("time left " + tRemainder);
+
+        // Minute Until Train
+        let tMinutesTillTrain = tFrequency - tRemainder;
+        console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+
+        // Next Train
+        let nextTrain = moment(moment().add(tMinutesTillTrain, "minutes")).format("hh:mm");
+
+        // Creates a table row containing the train parameters
         $("#trains").append(
             "<tr><td> " + childSnapshot.val().Name +
             " </td><td> " + childSnapshot.val().Dest +
-            " </td><td> " + childSnapshot.val().Time +
-            " </td><td> " + childSnapshot.val().Freq +
-            " </td></tr>"
+            // " </td><td> " + childSnapshot.val().Time +
+            // " </td><td> " + childSnapshot.val().Freq +
+            " </td><td> " + nextTrain +
+            " </td><td> " + tMinutesTillTrain + "</td></tr>"
         );
     }
 }
